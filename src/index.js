ba52b2ba7abbb4a7741a067da272695ca08f47f3
Adriana Rodriguez Sanabria <agrodriguezs09@gmail.com>
2020-04-14 21:34:55 +0200

    Fetch de API
    aplicacion para tomar los precios del bitcoint de un API, procesar el JSON y publicarlos usando Fetch con el React.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './PropTypesApp';
+import App from './sections/fetch-example';
 import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
