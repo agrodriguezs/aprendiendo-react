78d9e21f94fdce0a9afb8f16d7f633159c461bb7
Adriana Rodriguez Sanabria <agrodriguezs09@gmail.com>
2020-04-15 18:20:39 +0200

    Ciclo de Actualizacion de los Componentes en React
    Ejercicio de visualizacion de Animales.
    El ciclo de vida se puede dividir en 3 fases, el montado, actualización y desmontado del componente. Estas fases a su vez se dividen en varios métodos que puede tener el componente.

---
+++ src/CiclodeActualizacionApp.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import EjemplodeCicloDeActualizacion from './sections/ejciclodeactualizacion';
+
+class App extends Component {
+  render () {
+   return (
+        <div className="App">
+            <EjemplodeCicloDeActualizacion />
+        </div>
+    );
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './sections/fetch-example';
+import App from './CiclodeActualizacionApp';
 import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
