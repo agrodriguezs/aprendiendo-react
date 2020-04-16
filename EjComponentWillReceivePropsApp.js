5c14df3a7f3d1df56571142855aa1fbf7eea3f7b
Adriana Rodriguez Sanabria <agrodriguezs09@gmail.com>
2020-04-16 16:58:47 +0200

    ejemplo del ComponentWillReceiveProps
    Para seguir conociendo el Ciclo de Actualizacion de los componentes.

---
+++ EjComponentWillReceivePropsApp.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import EjcomponentWillReceiveProps from './sections/EjcomponentWillReceiveProps';
+
+class App extends Component {
+  render () {
+   return (
+        <div className="App">
+            <EjcomponentWillReceiveProps />
+        </div>
+    );
+  }
+}
+
+export default App;
