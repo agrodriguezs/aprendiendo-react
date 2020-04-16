c75f84f64ca2889f5fd4a627901c43dc606703bf
Adriana Rodriguez Sanabria <agrodriguezs09@gmail.com>
2020-04-16 19:43:19 +0200

    Ejemplo de Component WillUnmount, desmontar componentes y desvincular eventos ya no utilizados.

---
+++ src/EjComponentWillUnmountApp.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import EjemploDeComponentWillUnmount from './sections/EjComponentWillUnmount';
+
+class App extends Component {
+  render () {
+   return (
+        <div className="App">
+            <EjemploDeComponentWillUnmount />
+        </div>
+    );
+  }
+}
+
+export default App;
