adba90e5f0305e1f48cb4eddca118cb3917c85a3
Adriana Rodriguez Sanabria <agrodriguezs09@gmail.com>
2020-04-16 18:49:39 +0200

    Uso de PureComponent

---
+++ src/EjPureComponentApp.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import EjPureComponent from './sections/EjPureComponent';
+
+class App extends Component {
+  render () {
+   return (
+        <div className="App">
+            <EjPureComponent />
+        </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/sections/EjPureComponent.js
@@ -0,0 +1,64 @@
+import React, { Component, PureComponent } from 'react'
+import PropTypes from 'prop-types'
+
+const ANIMAL_IMAGES = {
+  panda: 'https://goo.gl/oNbtoq',
+  cat: 'https://goo.gl/PoQQXb',
+  dolphin: 'https://goo.gl/BbiKCd'
+}
+const ANIMALS = Object.keys(ANIMAL_IMAGES)
+
+class AnimalImage extends PureComponent {
+  state = { src: ANIMAL_IMAGES[this.props.animal] }
+
+  UNSAFE_componentWillReceiveProps (nextProps) {
+//     console.clear()
+    console.log('1. componentWillReceiveProps', nextProps)
+    this.setState({ src: ANIMAL_IMAGES[nextProps.animal] })
+  }
+
+  render () {
+    console.log('-> render')
+    return (
+      <div>
+        <p>Selected {this.props.animal}</p>
+        <img
+          alt={this.props.animal}
+          src={this.state.src}
+          width='250'
+        />
+      </div>
+    )
+  }
+}
+
+AnimalImage.propTypes = {
+  animal: PropTypes.oneOf(ANIMALS)
+}
+
+class EjPureComponent extends Component {
+  state = { animal: 'panda' }
+
+  _renderAnimalButton = (animal) => {
+    return (
+      <button
+        // disabled={animal === this.state.animal}
+        key={animal}
+        onClick={() => this.setState({ animal })}>
+        {animal}
+      </button>
+    )
+  }
+
+  render () {
+    return (
+      <div>
+        <h4>Ciclo de Actualización, Ejemplo de:PureComponent para evitar usar ShouldComponentUpdate</h4>
+        {ANIMALS.map(this._renderAnimalButton)}
+        <AnimalImage animal={this.state.animal} />
+      </div>
+    )
+  }
+}
+
+export default EjPureComponent
