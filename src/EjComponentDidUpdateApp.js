3b94b2f040b8ad9dd0475afe2e21b21d059df648
Adriana Rodriguez Sanabria <agrodriguezs09@gmail.com>
2020-04-16 19:19:37 +0200

    Ejemplo de ComponentDiidUpdate para completar Blur de Transicion.

---
+++ src/EjComponentDidUpdateApp.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import EjComponentDidUpdate from './sections/EjComponentDidUpdate';
+
+class App extends Component {
+  render () {
+   return (
+        <div className="App">
+            <EjComponentDidUpdate />
+        </div>
+    );
+  }
+}
+
+export default App;
