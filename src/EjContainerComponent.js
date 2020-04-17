7e54d234fc72bbe4b3aab60f831106645df746d5
Adriana Rodriguez Sanabria <agrodriguezs09@gmail.com>
2020-04-17 19:33:39 +0200

    Patron Contenedor Contenido Con el ejemplo de la visualizacion de los precios del bitcoin haciendo Fetch de una API

---
+++ src/EjContainerComponent.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import BitCoinPriceContainer from './sections/container-component';
+
+class App extends Component {
+  render () {
+   return (
+        <div className="App">
+            <BitCoinPriceContainer />
+        </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/sections/container-component/README.md
@@ -0,0 +1,5 @@
+Código fuente con el ejemplo de cómo usar las buenas prácticas de patrón-contenido.
+
+Lo que nos permite separar los componentes entre uno que tiene lógica y otro que sólo es presentacional.
+
+
